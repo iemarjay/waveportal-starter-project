2d1ae02a2416f34f4e3615a93db58c58896db797
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2022-09-06 04:20:48 +0400

    wave

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from "react";
-// import { ethers } from "ethers";
+import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/WavePortal.json";
 import {useEffect, useState} from "react";
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+
+  const contractAddress = "0xd5f08a0ae197482FA808cE84E00E97d940dBD26E";
+  const contractABI = abi.abi
 
   const checkIfWalletIsConnected = async () => {
     try{
@@ -50,8 +54,32 @@ export default function App() {
 
   }
 
-  const wave = () => {
+  const wave = async () => {
+    try {
+      const {ethereum} = window
 
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves()
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
